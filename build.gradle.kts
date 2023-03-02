f08a20aed0de8da64a1f5224e55d5cf396f11367
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-03-02 09:04:09 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "me.fapcs"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    api("me.fapcs:Shared:0.0.0")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "16"
+        }
+    }
+
+    build {
+        dependsOn("shadowJar")
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+    }
+}
+
+application {
+    mainClass.set("me.fapcs.stripe_controller.RelayAgent")
+}
